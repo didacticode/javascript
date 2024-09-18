25d7c1d72240633343048a0ea4845ac16304ab39
Didacticode <57577084+didacticode@users.noreply.github.com>
2024-09-18 09:01:23 +0000

    Resumen añadido

---
+++ 1_Sintaxis/00_resumen.js
@@ -0,0 +1,38 @@
+/* CARGA DE SCRIPTS */
+document.getElementById("saludo").innerHTML = prompt("¿Cómo te llamas?");
+
+/* window.onload = hasiera;
+
+function hasiera () {
+    document.getElementById("saludo").innerHTML = prompt("¿Cómo te llamas?");
+}*/
+
+/*const hasiera =  function () {
+    document.getElementById("saludo").innerHTML = prompt("¿Cómo te llamas?");
+}
+
+window.onload = hasiera;*/
+
+/* INSTRUCCIONES DE SALIDA */
+window.alert(2+3);
+document.write("<h2>¿Qué tal?</h2>");
+document.getElementById("miParrafo").innerHTML = 5/7;
+console.log(1+1);
+
+/* SENTENCIAS */
+// Varias sentencias en la misma línea separadas por ;
+// Se recomienda siempre punto y coma al final de la línea
+// Se recomienda utilizar espacios entre operadores: =. +, -, *...
+// Variables empiezan con letra, $ o _. 
+// Recomendación en creación de variables: lowerCamelCase
+// Mayúsculas en constantes y la inicial de clases
+   
+/* VARIABLES */
+"use strict";
+let nombre;
+const PI = 3.1416;
+
+/* OPERADORES */
+// OPERADORES ARITMÉTICOS: +, -, *, /, %, ++, --.
+			//OPERADORES DE ASIGNACIÓN: =, +=, -=, *=, /=, %=
+
